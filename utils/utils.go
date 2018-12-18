25741c04cfb015519e4943dd76e37bbea660a75e
030 <030@users.noreply.github.com>
2018-12-18 11:00:58 +0100

    [#3] string should be returned

--- utils/utils.go
+++ utils/utils.go
@@ -34,6 +34,6 @@ func Fatal(err error) {
 // This function ensures that paths are compatible with the OS, e.g.:
 // * a\b\c will be a/b/c on linux
 // * d/e/f will be d\e\f on Windows
-func CrossPlatformFilepath(p string) {
+func CrossPlatformFilepath(p string) string {
     return filepath.FromSlash(p)
 }
