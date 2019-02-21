376fb3903307a9be4890f3100a9b4b151211b186
030 <030@users.noreply.github.com>
2019-02-21 17:53:49 +0100

    set linenumber in log rus

--- utils/utils.go
+++ utils/utils.go
@@ -11,24 +11,12 @@ import (
 // Debug is a function that is able to enable debug logging
 func Debug() {
 	debug := flag.Bool("d", false, "Enable debug mode.")
-
 	flag.Parse()
+
+	logrus.SetReportCaller(true)
 
 	if *debug {
 		logrus.SetLevel(logrus.DebugLevel)
-	}
-}
-
-// ErrorLineNumber is a function that adds the linenumber to the error message
-func ErrorLineNumber() {
-	// https://stackoverflow.com/a/24809859/2777965
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-}
-
-// Fatal is a function that returns the error message and an exit code of 1
-func Fatal(err error) {
-	if err != nil {
-		log.Fatal(err)
 	}
 }
 
